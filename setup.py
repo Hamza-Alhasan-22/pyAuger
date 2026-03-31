65dd2dda175cd4da7fe899ffe3441ef470493b46
Hamza AlHasan <hamza.alhasan.22@gmail.com>
2026-03-31 00:19:28 -0400

    Edit setup

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ setup(
     version="1.0.0",
     description="Ab-initio direct Auger recombination calculator for semiconductors",
     author="Hamza Alhasan",
+    author_email="hamza.alhasan.22@gmail.com",
+    url="https://github.com/Hamza-Alhasan-22/pyAuger",
     packages=find_packages(),
     python_requires=">=3.8",
-    install_requires=[
-        "numpy",
-        "scipy",
-        "pandas",
-        "matplotlib",
-        "pymatgen",
-    ],
+    # install_requires=[
+    #     "numpy",
+    #     "scipy",
+    #     "pandas",
+    #     "matplotlib",
+    #     "pymatgen",
+    # ],
 )
